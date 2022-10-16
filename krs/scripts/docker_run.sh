86f7106661ca19e2cd6cfa3551412c2d6aa869f6
npz35 <npz35@noreply.git>
2022-10-16 15:45:40 +0900

    add options to docker run

--- krs/scripts/docker_run.sh
+++ krs/scripts/docker_run.sh
@@ -3,12 +3,15 @@
 USER_NAME=$USER
 
 docker run --interactive --tty \
-    --env DISPLAY=$DISPLAY \
     --net host \
+    --env DISPLAY=$DISPLAY \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume $HOME/.Xauthority:/home/${USER_NAME}/.Xauthority \
     --volume /tools/Xilinx:/tools/Xilinx:ro \
     --volume $HOME/output:/home/${USER_NAME}/output \
+    --volume /run/user/$(id -u)/pulse/native:/run/user/$(id -u)/pulse/native \
+    --env PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
+    --user $(id -u) \
     --name krs \
     kv260/krs:1.0 \
     /bin/bash
