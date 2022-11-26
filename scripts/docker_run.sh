3e9eac572c8246426df56c54726f7a152559d008
npz35 <npz35@noreply.git>
2022-11-26 14:30:13 +0900

    disable work

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,7 +2,9 @@
 
 USER_NAME=$USER
 
-mkdir -p work
+# mkdir -p work
+# --volume work:/home/${USER_NAME}/xilinx-kv260-starterkit-2022.1
+
 docker run --interactive --tty --privileged \
     --env DISPLAY=$DISPLAY \
     --net host \
@@ -10,7 +12,6 @@ docker run --interactive --tty --privileged \
     --volume $HOME/.Xauthority:/home/${USER_NAME}/.Xauthority \
     --volume /tools/Xilinx:/tools/Xilinx:ro \
     --volume $HOME/output:/home/${USER_NAME}/output \
-    --volume work:/home/${USER_NAME}/xilinx-kv260-starterkit-2022.1 \
     --name build_petalinux \
     kv260/petalinux-builder:20.04 \
     /bin/bash
