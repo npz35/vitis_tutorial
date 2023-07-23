6e4928d8b20a2eec895f2b7d5f1f6a544035a2de
npz35 <npz35@noreply.git>
2023-07-23 23:21:29 +0900

    add XILINX_TOOLS_INSTALL_DIR_PREFIX, rename output to krs_workdir_2023_1

--- krs/scripts/docker_run.sh
+++ krs/scripts/docker_run.sh
@@ -2,13 +2,19 @@
 
 USER_NAME=$USER
 
+if [ ! -d ${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx} ]; then
+    echo "${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx} does not existed." >&2
+    exit 1
+fi
+
 docker run --interactive --tty \
     --net host \
     --env DISPLAY=$DISPLAY \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume $HOME/.Xauthority:/home/${USER_NAME}/.Xauthority \
-    --volume /tools/Xilinx:/tools/Xilinx:ro \
-    --volume $HOME/output:/home/${USER_NAME}/output \
+    --volume ${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx}:/tools/Xilinx:ro \
+    --volume ${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx}:${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx}:ro \
+    --volume $HOME/krs_workdir_2023_1:/home/${USER_NAME}/krs_workdir_2023_1 \
     --volume /run/user/$(id -u)/pulse/native:/run/user/$(id -u)/pulse/native \
     --env PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
     --user $(id -u) \
