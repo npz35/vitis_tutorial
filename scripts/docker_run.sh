827ead7f2768e778ffe332a3a068e5af00bf936d
npz35 <npz35@noreply.git>
2023-07-23 16:50:44 +0900

    update XILINX_TOOLS_INSTALL_DIR_PREFIX

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,8 +2,8 @@
 
 USER_NAME=$USER
 
-if [ ! -d ${XILINX_TOOLS_INSTALL_DIR_PREFIX}/tools/Xilinx ]; then
-    echo "${XILINX_TOOLS_INSTALL_DIR_PREFIX}/tools/Xilinx does not existed." >&2
+if [ ! -d ${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx} ]; then
+    echo "${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx} does not existed." >&2
     exit 1
 fi
 
@@ -12,8 +12,8 @@ docker run --interactive --tty --privileged \
     --net host \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume $HOME/.Xauthority:/home/${USER_NAME}/.Xauthority \
-    --volume ${XILINX_TOOLS_INSTALL_DIR_PREFIX}/tools/Xilinx:/tools/Xilinx:ro \
-    --volume ${XILINX_TOOLS_INSTALL_DIR_PREFIX}/tools/Xilinx:${XILINX_TOOLS_INSTALL_DIR_PREFIX}/tools/Xilinx:ro \
+    --volume ${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx}:/tools/Xilinx:ro \
+    --volume ${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx}:${XILINX_TOOLS_INSTALL_DIR_PREFIX:-/tools/Xilinx}:ro \
     --volume $HOME/krs_workdir_2023_1:/home/${USER_NAME}/krs_workdir_2023_1 \
     --name build_petalinux \
     kv260/petalinux-builder:0.2.2 \
