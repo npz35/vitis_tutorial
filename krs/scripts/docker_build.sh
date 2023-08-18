756bc55196fa8dff6cb0f8f4abd9944c71a7b94c
npz35 <npz35@noreply.git>
2023-08-18 16:01:30 +0900

    set jobs

--- krs/scripts/docker_build.sh
+++ krs/scripts/docker_build.sh
@@ -4,6 +4,7 @@ docker build \
   --build-arg USER_NAME=$USER \
   --build-arg USER_UID=$(id -u) \
   --build-arg VERSION=2023.1 \
+  --build-arg VPP_PARALLEL=6 \
   -f Dockerfile \
   -t kv260/krs:0.2.2 \
   .
