3f2454daad2cec608ae1d2056e529f7c76475515
npz35 <npz35@noreply.git>
2023-08-18 17:29:57 +0900

    replace to petalinux-tools-docker

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-docker build \
-  --build-arg USER_NAME=$USER \
-  --build-arg USER_UID=$(id -u) \
-  --build-arg VERSION=2023.1 \
-  --build-arg INSTALLER_SUFFIX=05012318 \
-  -f Dockerfile \
-  -t kv260/petalinux-builder:0.4.0 \
-  .
+../petalinux-tools-docker/scripts/docker_build.sh
